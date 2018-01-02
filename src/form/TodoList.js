13549faa32e5291c7a760955627c32948b8d9086
Ricardo Pimienta <rpimienta@itexico.net>
2018-01-02 17:26:24 -0600

    tutorial 21: extracting Presentational Components

--- src/form/TodoList.js
+++ src/form/TodoList.js
@@ -56,89 +56,13 @@ console.log('Initial state:');
 console.log(store.getState());
 console.log('--------------');
 
-console.log('Dispatching ADD_TODO:');
-store.dispatch({
-  type:'ADD_TODO',
-  id:0,
-  text:'Learn React'
-})
-console.log('Current state:');
-console.log(store.getState());
-
-console.log('--------------');
-
-console.log('Dispatching ADD_TODO:');
-store.dispatch({
-  type:'ADD_TODO',
-  id:1,
-  text:'Learn Redux'
-})
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching ADD_TODO:');
-store.dispatch({
-  type:'ADD_TODO',
-  id:2,
-  text:'Forget Everything'
-})
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching TOGGLE_TODO:');
-store.dispatch({
-  type:'TOGGLE_TODO',
-  id:2
-})
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching ADD_TODO store 2:');
-store.dispatch({
-  type:'ADD_TODO',
-  id:3,
-  text:'Re-learn Everything'
-})
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching ADD_TODO store 2:');
-store.dispatch({
-  type:'ADD_TODO',
-  id:4,
-  text:'Not cry'
-})
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching TOGGLE_TODO for store 2:');
-store.dispatch({
-  type:'TOGGLE_TODO',
-  id:3
-})
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-store.dispatch({
-  type:'SET_VISIBILITY_FILTER',
-  filter: 'SHOW_ALL'
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
 
 const FilterLink = ({filter, currentFilter, children}) => {
   if(currentFilter === filter){
     return <span>{children}</span>
   }
   return(
-   <a href='#' 
+  <a href='#' 
     onClick={ (e)=>{
       e.preventDefault();
       store.dispatch({
@@ -147,24 +71,66 @@ const FilterLink = ({filter, currentFilter, children}) => {
       })
       console.log('Visibility '+ store.getState().visibilityFilter)
     }
-   }>
+  }>
     {children}
   </a>  
   );
 }
- const getVisibleTodos = ( todos , filter) => {
-  console.log(filter);
-  switch(filter){
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_ACTIVE':
-      return todos.filter( t => !t.completed);
-    case 'SHOW_COMPLETED':
-      return todos.filter( t => t.completed);
-    default: return todos;
-  }
- }
-let nextId = store.getState().todos[store.getState().todos.length -1].id;
+
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => {
+  <li 
+    onClick={ onClick }
+    style={{cursor:'pointer'}}
+  >
+    <p style={{textDecoration: completed? 'line-through': 'none'}}>{text}</p>
+  </li>
+}
+
+const TodoLists = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map( todo => 
+        <Todo
+          key= {todo.id}
+          {...todo}
+          onClick = {() => onTodoClick(todo.id)}
+        />
+    )}
+  </ul>
+  );
+
+const AddTodo = ({onAddClick}) => {
+  let input;
+  return(
+    <div>
+      <input type="text" ref={ node => { input = node}}/>
+        <button onClick={() => {
+          onAddClick(input.value);
+          input.value = ''  
+      }}>Add Todo</button>
+    </div>
+  );
+};
+
+const getVisibleTodos = ( todos , filter) => {
+console.log(filter);
+switch(filter){
+  case 'SHOW_ALL':
+    return todos;
+  case 'SHOW_ACTIVE':
+    return todos.filter( t => !t.completed);
+  case 'SHOW_COMPLETED':
+    return todos.filter( t => t.completed);
+  default: return todos;
+}
+}
+let nextId = 0;
 class TodoList extends Component{
   
   render(){
@@ -180,29 +146,22 @@ class TodoList extends Component{
     return(
       <div style={mainForm}>
         <h1>Todo List</h1>
-        <input type="text" ref={ node => { this.input = node}}/>
-        <button onClick={() => { store.dispatch({
-          type: 'ADD_TODO',
-          id:++nextId,
-          text: this.input.value
-        }) 
-        this.input.value = ''
-      }}>Add Todo</button>
-      <ul>
-        {
-          visibleTodos.map( todo => 
-            <li key={todo.id}
-              onClick={()=>{ store.dispatch({
-                type:'TOGGLE_TODO',
-                id: todo.id
-              })}}
-              style={{cursor:'pointer'}}
-            >
-              <p style={{textDecoration: todo.completed? 'line-through': 'none'}}>{todo.text}</p>
-            </li>
-          )
+        <AddTodo onAddClick={ text => 
+          store.dispatch({
+            type:'ADD_TODO',
+            id:nextId++,
+            text:text
+          })
+        }/>
+      <TodoLists
+        todos={visibleTodos}
+        onTodoClick={ id => 
+          store.dispatch({
+            type: 'TOGGLE_TODO',
+            id
+          })
         }
-      </ul>
+      />
       <p> Show: 
         {' '} 
         <FilterLink
